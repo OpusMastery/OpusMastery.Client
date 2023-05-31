31760c6e83eaf10a4b02cfe8f2e2676caad67a02
Rostyslav Nakarlovich <zvesdochyot@gmail.com>
2023-06-01 00:37:58 +0300

    Changed to HTML5 web history. Added extended routing to separate landing and portal pages

--- opusmastery-client/src/router/routes.ts
+++ opusmastery-client/src/router/routes.ts
@@ -4,27 +4,49 @@ const routes: RouteRecordRaw[] = [
     {
         path: '/',
         component: () => import('layouts/MainLayout.vue'),
-        children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+        meta: {
+            isAuthRequired: false,
+        },
+        children: [
+            {
+                path: '',
+                name: 'LandingPage',
+                component: () => import('pages/IndexPage.vue'),
+                meta: {
+                    title: 'OpusMastery Landing'
+                }
+            },
+            {
+                path: '/about',
+                name: 'AboutPage',
+                component: () => import('pages/landing/HelloWorldPage.vue'),
+                meta: {
+                    title: 'About us'
+                }
+            },
+            {
+                path: '/sign-in',
+                name: 'SignInPage',
+                component: () => import('pages/identity/LoginPage.vue'),
+                meta: {
+                    title: 'Sign-in to explore your Company'
+                }
+            }
+        ]
     },
     {
-        path: '/about',
-        component: () => import('pages/landing/HelloWorldPage.vue'),
+        path: '/some-portal-route',
+        component: () => import('layouts/MainLayout.vue'),
         meta: {
-            title: 'About us'
-        }
-    },
-    {
-        path: '/sign-in',
-        component: () => import('pages/identity/LoginPage.vue'),
-        meta: {
-            title: 'Sign-in to explore your Company'
-        }
+            isAuthRequired: true,
+        },
+        children: []
     },
     {
         path: '/:catchAll(.*)',
         component: () => import('pages/notFound/NotFoundPage.vue'),
         meta: {
-            title: '404 Not Found'
+            title: '404 - Please be mindful!'
         }
     }
 ];
